1db7990a32a44cb4df2101b34005d874be7d301a
Asmita Borawake <asmita.borawake@Sneha-Sures-Mac.local>
2019-10-23 17:54:01 +0530

    video with subtitle demo in swift

--- VideoWithSubtitleDemo/ViewController.swift
+++ VideoWithSubtitleDemo/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
-
+import MediaPlayer
+import AVKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        playVideoWithSubtitle()
     }
 
+    
+    func playVideoWithSubtitle(){
+        
+        // Video file
+        let videoFile = Bundle.main.path(forResource: "trailer_720p", ofType: "mov")
+        
+        // Local subtitle file
+        let subtitleFile = Bundle.main.path(forResource: "trailer_720p", ofType: "srt")
+        let subtitleURL = URL(fileURLWithPath: subtitleFile!)
+        
+        // Remote subtitle file
+        let subtitleRemoteUrl = URL(string: "https://raw.githubusercontent.com/furkanhatipoglu/AVPlayerViewController-Subtitles/master/Example/AVPlayerViewController-Subtitles/trailer_720p.srt")
+        
+        // Movie player
+        let moviePlayer = AVPlayerViewController()
+        moviePlayer.player = AVPlayer(url: URL(fileURLWithPath: videoFile!))
+        present(moviePlayer, animated: true, completion: nil)
+        
+        // Add subtitles - local
+         moviePlayer.addSubtitles().open(fileFromLocal: subtitleURL)
+         moviePlayer.addSubtitles().open(fileFromLocal: subtitleURL, encoding: .utf8)
+        
+        // Add subtitles - remote
+      //  moviePlayer.addSubtitles().open(fileFromRemote: subtitleRemoteUrl!)
+        
+        // Change text properties
+        moviePlayer.subtitleLabel?.textColor = UIColor.red
+        
+        // Play
+        moviePlayer.player?.play()
+    }
 
 }
 
